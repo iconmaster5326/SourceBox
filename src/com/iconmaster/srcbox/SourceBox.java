60ce22adda271aa73584733c86c7b6d3b01a1c00
Iconmaster <crazybonesj@yahoo.com>
2015-01-26 13:44:22 -0500

    Added back in SourceBoxCore wrapper code

--- src/com/iconmaster/srcbox/SourceBox.java
+++ src/com/iconmaster/srcbox/SourceBox.java
@@ -3,7 +3,9 @@ package com.iconmaster.srcbox;
 import com.iconmaster.source.assemble.AssembledOutput;
 import com.iconmaster.source.link.Platform;
 import com.iconmaster.source.link.platform.PlatformLoader.LoadedPlatform;
+import com.iconmaster.source.prototype.Function;
 import com.iconmaster.source.prototype.SourcePackage;
+import com.iconmaster.srcbox.execute.VirtualMachine;
 
 /**
  *
@@ -35,6 +37,12 @@ public class SourceBox extends Platform {
 
 	@Override
 	public Object run(SourcePackage pkg) {
+		VirtualMachine vm = new VirtualMachine(pkg);
+		Function main = SourceBoxCore.getMainFunction(pkg);
+		if (main!=null) {
+			vm.loadFunction(main);
+			vm.run();
+		}
 		return null;
 	}
 }
